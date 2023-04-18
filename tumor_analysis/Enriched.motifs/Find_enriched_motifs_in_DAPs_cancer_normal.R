3fb86c4bd2212011bd20c617a43ae1165516d0e5
Alla Karpova <allakarpova@allas-mbp.wucon.wustl.edu>
2023-04-18 14:51:00 -0500

    make usable in meged object

--- tumor_analysis/Enriched.motifs/Find_enriched_motifs_in_DAPs_cancer_normal.R
+++ tumor_analysis/Enriched.motifs/Find_enriched_motifs_in_DAPs_cancer_normal.R
@@ -165,7 +165,6 @@ if (cancer.type=='BRCA' | cancer.type=='BRCA_Basal' ) {
   depleted.m <- findDepletedMotifs(cancer=cancer.type, open.peaks)
   fwrite(depleted.m, glue::glue('Motifs_depleted_in_{cancer.type}_cancer_vs_{normal}.tsv'), row.names = F, sep='\t')
 
-  
 } else {
     normal <- cancer.normal.pairs %>% filter(Cancer==cancer.type) %>% pull(Normal)
     open.peaks <- AccessiblePeaks(obj, idents = c(glue::glue("{cancer.type}__Tumor"), normal))
