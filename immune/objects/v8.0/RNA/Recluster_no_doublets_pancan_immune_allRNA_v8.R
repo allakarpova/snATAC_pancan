00bdeb25fc1b12e1d43abd4f23b561bd2b1d15c6
Alla Karpova <allakarpova@allas-mbp.wucon.wustl.edu>
2023-08-25 13:59:32 -0500

    Added removal of cancer genes from integration features

--- immune/objects/v8.0/RNA/Recluster_no_doublets_pancan_immune_allRNA_v8.R
+++ immune/objects/v8.0/RNA/Recluster_no_doublets_pancan_immune_allRNA_v8.R
@@ -179,7 +179,7 @@ all.rna.list <- lapply(X = all.rna.list, FUN = function(x) {
 
 message('Selecting integration features')
 features <- SelectIntegrationFeatures(object.list = all.rna.list, nfeatures = 3000, )
-
+print(length(features))
 if(opt$remove.tumor.genes) {
   tumor.genes <- fread('/diskmnt/Projects/snATAC_analysis/immune/obj/v8.0/auxiliary/DEGs_tumor_T-cells/Degs_tumor_vs_Tcells_all.cancers.tsv', data.table = F) 
   suspicious <- tumor.genes %>% 
@@ -194,11 +194,12 @@ if(opt$remove.tumor.genes) {
            max.pct.2 = max(pct.2)) %>% 
     filter(min.pct.2 < 0.1) %>% 
     pull(gene) %>% unique 
-  
-  length(suspicious)
-  length(trash.genes)
+
+  print(length(intersect(features, trash.genes)))
+  print(length(setdiff(features, trash.genes)))
   features <- setdiff(features, trash.genes)
 }
+print(length(features))
 
 all.rna.list <- PrepSCTIntegration(object.list = all.rna.list, anchor.features = features)
 message('Run PCA on integration features')
