a0cf6e5c17bbea633a4563de9387c3f52a22629a
Alla Karpova <allakarpova@allas-mbp.wucon.wustl.edu>
2023-09-18 11:00:26 -0500

    Scripts to run LinkPeaksGenes

---
+++ immune/analysis/linking_genes/Link_peaks_to_genes.R
@@ -0,0 +1,70 @@
+# Alla Karpova run LinkPeaks on a multiome object immune cells
+
+suppressMessages(library(Signac))
+suppressMessages(library(Seurat))
+suppressMessages(library(tidyverse))
+suppressMessages(library(data.table))
+suppressMessages(library(future))
+plan("multicore", workers =10)
+options(future.globals.maxSize = 50 * 1024^3)
+suppressMessages(library(optparse))
+
+suppressMessages(library(BSgenome.Hsapiens.UCSC.hg38))
+
+
+###options###
+######################
+option_list = list(
+  make_option(c("-i", "--input"),
+              type="character",
+              default=NULL, 
+              help="input object",
+              metavar="character"),
+  make_option(c("-o", "--output"),
+              type="character",
+              default="./", 
+              help="output folder path",
+              metavar="character")
+  
+);
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser)
+
+# read in initial arguments
+input_path <- opt$input
+out_path <- opt$output
+
+dir.create(out_path, showWarnings = F)
+setwd(out_path)
+
+dir.create('out')
+dir.create('plots')
+
+out.obj <- str_replace(input_path, pattern = 'rds', replacement = 'linked.rds')
+out.obj <- str_split(out.obj, '[/]')[[1]][str_count(out.obj, '/')+1]
+
+cat('opening object \n')
+obj <- readRDS(input_path)
+DefaultAssay(obj) <- "pancan"
+cat('done \n')
+
+min.cells.num <- 0.00005*ncol(obj)
+print(min.cells.num)
+
+# first compute the GC content for each peak
+obj <- RegionStats(obj, genome = BSgenome.Hsapiens.UCSC.hg38)
+
+# link peaks to genes
+obj <- LinkPeaks(
+  object = obj,
+  peak.assay = "pancan",
+  distance = 5e+05,
+  n_sample = 1000,
+  #min.cells = min.cells.num,
+  expression.assay = "SCT"
+)
+
+
+print(out.obj)
+saveRDS(obj, paste0('out/', out.obj))
